c32bb20fe3d9b0187b964327c07fd6cc24ab3640
liver1892 <liverynwa1897@gmail.com>
2026-02-21 22:09:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "blue";
-ctx.fillRect(50, 50, 100, 100);
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+
+// 落ちるブロック
+let block = {
+  x: 4,
+  y: 0
+};
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "blue";
+  ctx.fillRect(
+    block.x * BLOCK_SIZE,
+    block.y * BLOCK_SIZE,
+    BLOCK_SIZE,
+    BLOCK_SIZE
+  );
+}
+
+function update() {
+  block.y++;
+  draw();
+}
+
+// 0.5秒ごとに落ちる
+setInterval(update, 500);
